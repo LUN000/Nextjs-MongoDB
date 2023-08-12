21fe8f9ff3948b77e64c06c5a0f12349606a086c
LUN000 <x0933387310@gmail.com>
2023-08-13 07:55:14 +0800

    add GET DELETE API

--- app/api/member/route.js
+++ app/api/member/route.js
@@ -1,6 +1,6 @@
 import connectMongoDB from '@/libs/mongodb';
 import Member from '@/models/member';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request) {
     const { email, psswd, verify } = await request.json();
@@ -9,16 +9,29 @@ export async function POST(request) {
     return NextResponse.json({ message: 'Member Created' }, { status: 201 });
 }
 
-// export async function GET(request) {
-//     const { email, psswd, verify } = await request.json();
-//     await connectMongoDB();
-//     await Member.create({ email, psswd, verify });
-//     return NextResponse.json({ message: 'Member Created' }, { status: 201 });
-// }
+export async function GET(request) {
+    const id = request.nextUrl.searchParams.get("id");
+    await connectMongoDB();
+    if( id != "" ){
+        var members = await Member.findById(id);
+    } else {
+        var members = await Member.find({});
+    };
 
-// export async function DELETE(request) {
-//     const { email, psswd, verify } = await request.json();
-//     await connectMongoDB();
-//     await Member.deleteOne({ email, psswd, verify });
-//     return NextResponse.json({ message: 'Member Deleted' }, { status: 201 });
-// }
+    if( members === null ){
+        return NextResponse.json({ message: 'No Match id!' }, { status: 200 });
+    } else {
+        return NextResponse.json({ members }, { status: 200 });
+    };
+}
+
+export async function DELETE(request) {
+    const id = request.nextUrl.searchParams.get("id");
+    await connectMongoDB();
+    const del = await Member.findByIdAndDelete(id);
+    if(del === null){
+        return NextResponse.json({ message: 'No Match id!' }, { status: 200 });
+    } else {
+        return NextResponse.json({ message: 'Member Deleted' }, { status: 200 });
+    };
+}
