167c2de4f7d2484077faf7aab23892562b0f9899
LUN000 <x0933387310@gmail.com>
2023-08-13 15:55:54 +0800

    used email error exception

--- app/api/member/route.ts
+++ app/api/member/route.ts
@@ -1,13 +1,20 @@
 import connectMongoDB from '@/libs/mongodb';
 import Member from '@/models/member';
 import { NextResponse, NextRequest } from 'next/server';
-import { JsonMinusNumericLiteral } from 'typescript';
 
 export async function POST(request: NextRequest) {
-    const { email, psswd, verify } = await request.json();
-    await connectMongoDB();
-    await Member.create({ email, psswd, verify });
-    return NextResponse.json({ message: 'Member Created' }, { status: 201 });
+    try {
+        const { email, psswd, verify } = await request.json();
+        await connectMongoDB();
+        await Member.create({ email, psswd, verify });
+        return NextResponse.json({ message: 'Member Created' }, { status: 201 });
+    } catch ( err ) {
+        if( err.code === 11000 ) {
+            return NextResponse.json({ error: 'Duplicate email', message: 'Email address been used' }, { status: 400 });
+        } else {
+            return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+        }
+    }
 }
 
 export async function GET(request: NextRequest) {
@@ -32,7 +39,7 @@ export async function DELETE(request: NextRequest) {
     const id = request.nextUrl.searchParams.get("id");
     await connectMongoDB();
     const del = await Member.findByIdAndDelete(id);
-    
+
     if(del === null){
         return NextResponse.json({ message: 'No Match id!' }, { status: 200 });
     } else {
